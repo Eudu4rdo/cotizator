b40b6c05d9409f63b753ab726bd660946b62de25
Eduardo Garcia <eg47202@gmail.com>
2025-08-27 20:14:18 -0300

    feat: incluindo arquivo de boot da aplicação

---
+++ src/main/java/br/com/eduardo/cotizator/CotizatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.cotizator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotizatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotizatorApplication.class, args);
+	}
+
+}
