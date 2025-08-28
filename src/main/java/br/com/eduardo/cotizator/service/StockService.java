0f723e93c087ecc96ae423a3dcc00078831886a8
Eduardo Garcia <eg47202@gmail.com>
2025-08-28 11:10:35 -0300

    refactor (service) : ajustando o service para permitir que seja usado mock para os testes

--- src/main/java/br/com/eduardo/cotizator/service/StockService.java
+++ src/main/java/br/com/eduardo/cotizator/service/StockService.java
@@ -11,14 +11,18 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class StockService {
-    @Value("${b3.api.url}")
-    private String api_url;
-    @Value("${b3.api.token}")
-    private String api_token;
+    private final String api_url;
+    private final String api_token;
+    private final RestTemplate restTemplate;
+
+    public StockService(@Value("${b3.api.url}") String api_url, @Value("${b3.api.token}") String api_token, RestTemplate restTemplate) {
+        this.api_url = api_url;
+        this.api_token = api_token;
+        this.restTemplate = restTemplate;
+    }
 
     public StockApiResponse getStockData(String cod) {
         String url = this.api_url + "quote/" + cod;
-        RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + this.api_token);
         HttpEntity<String> entity = new HttpEntity<>(headers);
@@ -42,15 +46,12 @@ public class StockService {
         }
     }
 
-    public Integer CalculateQtd(double stock_value, double total_amount, double stock_percentage, boolean strict_limit) {
+    public Integer calculateQtd(double stock_value, double total_amount, double stock_percentage, boolean strict_limit) {
         double stock_amount = total_amount * stock_percentage;
-        int stock_qtd;
         if (strict_limit) {
-            stock_qtd = (int) Math.ceil(stock_amount / stock_value);
+            return (int) Math.floor(stock_amount / stock_value);
         } else {
-            stock_qtd = (int) Math.floor(stock_amount / stock_value);
+            return (int) Math.ceil(stock_amount / stock_value);
         }
-        return stock_qtd;
     }
-
 }
